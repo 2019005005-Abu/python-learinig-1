bb31718cdb8cbcd7e8416d63b8fb59ef825eb0e3
Abu Al Shahriar Rifat <rifatdiu2686@gmail.com>
2022-09-08 17:53:30 +0600

    first commit

---
+++ 1.py
@@ -0,0 +1,105 @@
+##Identifier
+from asyncio import constants
+
+
+var_1=190
+print(var_1);
+var_=2000;
+print(var_);
+help('keywords');
+print('I am a full stack Python developer');
+print("Hello\nWelcome");
+
+##operators
+'''
+
+
+    1.Arithmetic operators
+    2.Assignment operators
+    3.Comparison operators
+    4.Logical operators
+    5.Identity operators
+    6.Membership operators
+    7.Bitwise operators
+
+'''
+
+'''
+Based an operator
+1.Arithmetic Operator
+2.Relational Operator
+3.Membership Operator
+4.Boolean Operator
+
+##Arithmetic Operator
+1.Negation
+2.Addition
+3.Subtraction
+4.Multiplication
+5.Division
+6.Truncating div
+7.Modulus
+8.Exponential
+'''
+print(-5);
+print(1-+20);
+print(45-34);
+print(78*56);
+print(67//56);
+print(67/56);
+print(5%2);
+
+'''
+
+1.Equal --->  ==
+2.Not Equal --> !=
+3.Less than --> <
+4.Greater than --> >
+5.Less than or equal to -- >=
+6.greater than or  equal to -- >=
+
+'''
+print(78>67);
+print(100==100);
+
+'''
+1.Membership Operator
+=>1.in
+=>2.not in
+
+
+2.Boolean Operator
+=>1.and
+=>2.Or
+=>not
+'''
+
+isValue=10 in [1,2,3,10,20];
+print(isValue);
+isNoTValue=6  in [10,78,45,89];
+print(isNoTValue);
+print(10<100 or 67<45);
+print(10>5 and 100<102);
+'''
+presendence 
+1.()
+2.**
+3.Unary operator
+4.*,/,//,%
+5.+,-
+6.<<,>>
+7.&
+8.^
+9.|
+10.relational and membership operator
+11.not
+12.and
+13.or
+'''
+print((2+5)*4);
+print(2+5-1);
+print(3**4);
+print(2**3);
+
+
+
